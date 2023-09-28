eeaff657ea8595e1b18d1a80367874506d6bfe8c
Matt Russell <mrussell@cs.tufts.edu>
2023-09-28 12:18:14 -0400

    rm pointer from main

--- battleship/main.cpp
+++ battleship/main.cpp
@@ -1,12 +1,13 @@
-//main.cpp
-//by matt russell
-//2019
+/* 
+  main.cpp
+  by matt russell
+  2019
+*/
 
 #include "battleship.h"
 
 int main(){
-  battleship *b = new battleship;
-  b->run_game();
-  delete b;
-  b = NULL;
+  battleship b;
+  b.run_game();
+  return 0;
 }
